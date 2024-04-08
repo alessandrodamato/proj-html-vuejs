44bb46fc52dbdb147392bc5353ccee0151f9a7d7
Alessandro D'Amato <151948578+alessandrodamato@users.noreply.github.com>
2024-04-08 12:03:17 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+import { store } from './data/store'
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
+  export default {
+    components:{
+      Header,
+      Main,
+      Footer
+    },
+
+    data(){
+      return{
+        store
+      }
+    }
+  }
+</script> 
+
+
+
+<template>
+
+  <Header />
+
+  <Main />
+  
+  <Footer />
+
+</template>
+
+
+
+<style lang="scss">
+
+@import url(./assets/scss/main.scss);
+
+</style>
