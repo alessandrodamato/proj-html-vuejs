5b5d91b4aa3db496f40056a111547a5ea254a937
alessandrodamato <alessandro042004@gmail.com>
2024-04-08 13:24:06 +0200

    [add] font awesome cdn & header template

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,7 +9,38 @@
 <template>
 
   <header>
-    h
+
+    <div class="header-top">
+
+      <div class="ad_container-sm container-fluid">
+        
+        <div class="row">
+
+          <div class="col">a</div>
+          <div class="col-auto">b</div>
+
+        </div>
+
+      </div>
+      
+    </div>
+
+    <div class="header-bottom">
+
+      <div class="ad_container-sm container-fluid">
+        
+        <div class="row row-cols-3">
+
+          <div class="col">a</div>
+          <div class="col">b</div>
+          <div class="col">c</div>
+
+        </div>
+
+      </div>
+
+    </div>
+
   </header>
   
 </template>
@@ -17,5 +48,21 @@
 
 
 <style lang="scss" scoped>
+
+@use '../assets/scss/partials/vars' as *;
+
+  header{
+
+    .header-top{
+      height: 40px;
+      color: $fill-white;
+      background: linear-gradient(to left, $gray-3, #3b3b3b);
+    }
+
+    .header-bottom{
+      height: 105px;
+    }
+
+  }
 
 </style>
